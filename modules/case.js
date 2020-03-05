6213d4e6597c307db01cac0ef716cf63d47578e2
tomskarlismiezis <59993417+tomskarlismiezis@users.noreply.github.com>
2020-03-05 14:47:03 +0200

    Update case.js

--- modules/case.js
+++ modules/case.js
@@ -15,11 +15,11 @@ exports.execute = (req, res) => {
     var version = 'v0';
     var baseString = version + ':' + timestamp + ':' + requestBody;
     var hashedString = version + '=' + hmac.digest('hex');
-
-    if (hashedString != req.headers['X-Slack-Signature']) {
-        res.send("Invalid token");
-        return;
-    }
+    //if (!crypto.timingSafeEqual(hashedString, req.headers['X-Slack-Signature'])) {
+        console.log(hashedString + ' != ' + req.headers['X-Slack-Signature']);
+        //res.send("Invalid token");
+        //return;
+    //}
 
     let slackUserId = req.body.user_id,
         oauthObj = auth.getOAuthObject(slackUserId),
