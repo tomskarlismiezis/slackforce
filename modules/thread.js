7e2c4d503b710294d8346f50f99910ff1a7bdd7a
tomskarlismiezis <59993417+tomskarlismiezis@users.noreply.github.com>
2020-03-24 12:50:33 +0200

    Update thread.js

--- modules/thread.js
+++ modules/thread.js
@@ -4,7 +4,9 @@ let auth = require("./slack-salesforce-auth"),
     force = require("./force"),
     verif = require("./verifying"),
     crypto = require("crypto"),
+    request = require("request"),
     SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
+
 
 exports.execute = (req, res) => {
     let payload = JSON.parse(req.body.payload),
@@ -19,7 +21,8 @@ exports.execute = (req, res) => {
     let replies = payload.message.replies,
         children = '',
         thread_id = payload.message.thread_ts,
-        parent = false;
+        parent = false,
+        response_url = payload.response_url;
 
     //if (thread_id == payload.message.ts){
     //    thread_id = '';
@@ -48,15 +51,18 @@ exports.execute = (req, res) => {
                     {color: "#F2CF5B", fields: fields}
                 ]
             };
-            res.json(message);
+            request.post(response_url, message);
+            //res.json(message);
         
         })
         .catch((error) => {
-            console.log(error);
             if (error.code == 401) {
-                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
+                //res.send();
+                
+                request.post(response_url, `Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
 
             } else {
+                console.log(error);
                 res.send("An error as occurred");
             }
         });
